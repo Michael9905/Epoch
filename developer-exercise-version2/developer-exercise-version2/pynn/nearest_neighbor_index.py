60e6f7450167a8fd7de8fab8f838d7560c0d210b
Michael B. <161143403+Michael9905@users.noreply.github.com>
2024-10-15 11:48:50 -0400

    Update nearest_neighbor_index.py

--- developer-exercise-version2/developer-exercise-version2/pynn/nearest_neighbor_index.py
+++ developer-exercise-version2/developer-exercise-version2/pynn/nearest_neighbor_index.py
@@ -24,16 +24,27 @@ class NearestNeighborIndex:
         points, None is returned.
         """
 
+        """
+        A slow method for finding the nearest neighbor by checking all points.
+
+        :param query_point: The point (x, y) for which the nearest neighbor is searched.
+        :param haystack: A list of 2D points to search from.
+
+        :return: The point from `haystack` that is closest to `query_point`.
+        """
+
         min_dist = None
         min_point = None
 
-        #d
+        # Loop through each 2D points to find the nearest points
         for point in haystack:
+
+            # (x 2 − x 1 ) - ( y 2 − y 1 )
             x = point[0] - query_point[0]
             y = point[1] - query_point[1]
 
             # No need to use Euclidean formula by squaring
-            # Instead: (x^2 − x 1 ) * 2 + ( y^2 − y 1 ) * 2
+            # Instead: (x^2 ) * 2 + ( y^2 ) * 2
             dis_sq = x ** 2 + y ** 2
 
             if min_dist is None or dis_sq < min_dist:
@@ -42,16 +53,21 @@ class NearestNeighborIndex:
 
         return min_point
 
-
-
     def find_nearest_fast(self, query_point):
         """
         returns the point that is closest to query_point.
         If there are no indexed points, None is returned.
         """
 
+        """  
+        A faster method for finding the nearest neighbor by reducing unnecessary computations.
+        The current implementation is still brute-force, but it avoids calculating the square root
+        for each distance check, improving performance.
+        
+        :param query_point: The point (x, y) for which the nearest neighbor is searched.
+        :return: The closest point from the index.
+        """
 
-        #new
         min_dist_sq = float('inf')
         min_point = None
 
